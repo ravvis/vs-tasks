401acd26e5eb7ca96afed876a73323f897db23af
singh-ravi-siso <ravi16iiitg@gmail.com>
2021-03-07 22:37:35 +0530

    removing everything

--- babel.config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  "sourceType": "module"
-};
--- webviews/pages/HelloWorld.ts
+++
@@ -1,8 +0,0 @@
-import Vue from '../vue';
-import HelloWorld from "../components/HelloWorld.vue";
-console.log("hello from the ts file");
-console.log({ vue: Vue });
-new Vue({
-  el: '#vue-project-here',
-  render: (h: any) => h(HelloWorld),
-});
